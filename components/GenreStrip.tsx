9e99851cb019642c9b3dfe085c0a7e7b7d44dead
Rafael Turse <rribeiro@dynacert.com>
2026-09-09 07:51:01 -0400

    icons

--- components/GenreStrip.tsx
+++ components/GenreStrip.tsx
@@ -14,7 +14,7 @@ const AdventureIcon = ({ size }: IconProps) => (
   </svg>
 );
 
-const RpgIcon = ({ size }: IconProps) => (
+const WarIcon = ({ size }: IconProps) => (
   <svg width={size * 1.3} height={size * 1.3 * (512 / 640)} viewBox="0 0 640 512" fill="currentColor">
     <path d="M528 448H112c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h416c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm64-320c-26.5 0-48 21.5-48 48 0 7.1 1.6 13.7 4.4 19.8L476 239.2c-15.4 9.2-35.3 4-44.2-11.6L350.3 85C361 76.2 368 63 368 48c0-26.5-21.5-48-48-48s-48 21.5-48 48c0 15 7 28.2 17.7 37l-81.5 142.6c-8.9 15.6-28.9 20.8-44.2 11.6l-72.3-43.4c2.7-6 4.4-12.7 4.4-19.8 0-26.5-21.5-48-48-48S0 149.5 0 176s21.5 48 48 48c2.6 0 5.2-.4 7.7-.8L128 416h384l72.3-192.8c2.5.4 5.1.8 7.7.8 26.5 0 48-21.5 48-48s-21.5-48-48-48z" />
   </svg>
@@ -28,7 +28,7 @@ const RomanceIcon = ({ size }: IconProps) => (
   </svg>
 );
 
-const WarIcon = ({ size }: IconProps) => (
+const RpgIcon = ({ size }: IconProps) => (
   <svg width={size} height={size} viewBox="0 0 24 24" fill="currentColor">
     <g transform="translate(12 12) scale(1.15) translate(-12 -12)">
       <path d="M4 5a1 1 0 0 1 .3 -.714a6 6 0 0 1 8.213 -.176l.351 .328a4 4 0 0 0 5.272 0l.249 -.227c.61 -.483 1.527 -.097 1.61 .676l.005 .113v9a1 1 0 0 1 -.3 .714a6 6 0 0 1 -8.213 .176l-.351 -.328a4 4 0 0 0 -5.136 -.114v6.552a1 1 0 0 1 -1.993 .117l-.007 -.117v-16z" />
